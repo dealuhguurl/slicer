d26bef1cb8ba2bf467950f90ca791315edb8a9ef
dealuhguurl <dealuhguurl@gmail.com>
2024-07-30 19:02:29 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env::*, process::*};
+use http::*;
+
+fn url_shortener(url: String) {
+    println!("URL: {url}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: cargo run <url>");
+        std::process::exit(1);
+    }
+    url_shortener(args[1].clone());
 }
